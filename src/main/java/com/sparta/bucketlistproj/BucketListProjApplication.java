796f3427d40f05f1eee7ea73fb31b5a021c2c73f
uijin <powerlife145@gmail.com>
2023-07-19 09:54:01 +0900

    CORS 해결

--- src/main/java/com/sparta/bucketlistproj/BucketListProjApplication.java
+++ src/main/java/com/sparta/bucketlistproj/BucketListProjApplication.java
@@ -2,12 +2,12 @@ package com.sparta.bucketlistproj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin
 public class BucketListProjApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BucketListProjApplication.class, args);
 	}
-
 }
